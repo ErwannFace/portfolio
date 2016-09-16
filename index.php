af52ffbd938debbc391c651ddf0f51277e8c5089
root <root@redfish-3>
2016-09-16 14:25:27 +0200

    link with the good link

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 </head>
 <body>
 
-<p><a href="https://github.com/JeanSairien/portfolio.git">My red-folio</a></p>
+<p><a href="https://github.com/JeanSairien/redfolio.git">My red-folio</a></p>
 
 </body>
 </html>
